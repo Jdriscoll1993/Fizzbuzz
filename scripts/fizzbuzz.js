ba22dc661b73b3395d1c2e75f8dbc530d9b9045b
Joey Driscoll <driscoll.recording@gmail.com>
2019-08-01 21:37:49 -0500

    2 more solutions

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -2,6 +2,7 @@
 
 //            !FIZZ BUZZ!
 
+// APRIL 2019
 
 for (let i = 1; i < 101; i++) {
     if (i % 35 === 0) console.log("chickenmonkey");
@@ -12,6 +13,7 @@ for (let i = 1; i < 101; i++) {
 
 //------------------------------------------------------//
 
+// APRIL 2019
 
 for (let i = 1; i < 101; i++) {
 
@@ -27,3 +29,22 @@ for (let i = 1; i < 101; i++) {
 
 //------------------------------------------------------//
 
+// AUGUST 2019
+for(let i = 1; i < 101; i++){
+    if(i % 15 === 0){
+        console.log("fizzbuzz");
+    } else if(i % 5 === 0){
+        console.log("buzz");
+    } else if(i % 3 === 0){
+        console.log("fizz");
+    }else{
+        console.log(`${i}`);
+    }
+}
+
+//------------------------------------------------------//
+
+// AUGUST 2019
+
+for(let i = 0; i < 100;)
+    console.log((++i % 3 ? '' : 'fizz')+(i % 5 ? '' : 'buzz')||i);
